d73467b0e1c2d841a56c26ca0d1ceda6f3e76c49
Björn Sundin <bjornsundin02@gmail.com>
2023-12-23 13:05:37 +0100

    Use std::size_t in c++, AAA

--- tests/http_response_parser_callbacks.cpp
+++ tests/http_response_parser_callbacks.cpp
@@ -76,7 +76,7 @@ void test_callbacks_full_input(
 	};
 
 	for (auto const chunk_size : chunk_sizes_to_test) {
-		size_t number_of_parsed_packets = 0;
+		auto number_of_parsed_packets = std::size_t{};
 
 		auto response_callbacks = algorithms::ResponseCallbacks{
 			.handle_raw_progress = [&](ResponseProgressRaw& progress) {
@@ -105,7 +105,7 @@ void test_callbacks_full_input(
 		};
 		auto const result = parse_input_in_chunks(algorithms::ResponseParser{response_callbacks}, input_string, chunk_size);
 		CHECK(result == expected_result);
-		CHECK(number_of_parsed_packets <= static_cast<size_t>(std::ceil(static_cast<double>(input_string.size()) / static_cast<double>(chunk_size))));
+		CHECK(number_of_parsed_packets <= static_cast<std::size_t>(std::ceil(static_cast<double>(input_string.size()) / static_cast<double>(chunk_size))));
 	}
 }
 
@@ -130,7 +130,7 @@ void test_callbacks_stopping_after_head(
 	};
     
     for (auto const chunk_size : chunk_sizes_to_test) {
-		size_t number_of_parsed_packets = 0;
+		auto number_of_parsed_packets = std::size_t{};
 		auto got_any_body = false;
         auto response_callbacks = algorithms::ResponseCallbacks{
             .handle_raw_progress = [&](ResponseProgressRaw& progress) {
@@ -161,7 +161,7 @@ void test_callbacks_stopping_after_head(
 			expected_result
 		);
 		CHECK(!got_any_body);
-		CHECK(number_of_parsed_packets == static_cast<size_t>(std::ceil(static_cast<double>(headers_string.size() + header_body_separator.size()) / 
+		CHECK(number_of_parsed_packets == static_cast<std::size_t>(std::ceil(static_cast<double>(headers_string.size() + header_body_separator.size()) / 
 			static_cast<double>(chunk_size))));
     }
 }
